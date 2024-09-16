3939ae0b0897796dd56e2b2f36f63b839903380d
Olivia Giles <oliviagiles@olivias-air-82.lan>
2024-09-15 22:48:51 -0600

    Pois Reg included

--- Bike.R
+++ Bike.R
@@ -4,6 +4,7 @@ library(patchwork)
 library(DataExplorer)
 library(GGally)
 library(tidymodels)
+library(poissonreg)
 
 #Read in Code
 data_train <- vroom("Stat348/BikeShare/train.csv")
@@ -41,11 +42,11 @@ graph4
 (graph1 + graph2)/(graph3 + graph4)
 
 #Linear Regression
-# Setup and Fit the Linear Regression Model3
+# Setup and Fit the Linear Regression Model
 my_linear_model <- linear_reg() %>% #Type of model
   set_engine("lm") %>% # Engine = What R function to use
   set_mode("regression") %>% # Regression just means quantitative response6
-  fit(formula=count~atemp + weather + humidity + windspeed + temp, data=data_train)
+  fit(formula=count~temp + humidity + windspeed, data=data_train)
 
 ## Generate Predictions Using Linear Model
 bike_predictions <- predict(my_linear_model,
@@ -62,3 +63,29 @@ bind_cols(., data_test) %>% #Bind predictions with test data
 ## Write out the file
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
 
+
+#Poisson Regression
+data_train$season <- as.factor(data_train$season)
+data_test$season <- as.factor(data_test$season)
+data_train$weather <- as.factor(data_train$weather)
+data_test$weather <- as.factor(data_test$weather)
+#Modeling
+pois_model <- poisson_reg() %>% 
+  set_engine("glm") %>% 
+  set_mode("regression") %>% 
+  fit(formula = count ~ temp + humidity + windspeed + season + weather, data = data_train)
+
+#Predictions for poisson
+bike_pred <- predict(pois_model,
+                     new_data = data_test)
+bike_pred
+
+#Kaggle Submission for Poisson
+kaggle_submission <- bike_predictions %>%
+  bind_cols(., data_test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+## Write out the file
+vroom_write(x=kaggle_submission, file="./PoisPreds2.csv", delim=",")
